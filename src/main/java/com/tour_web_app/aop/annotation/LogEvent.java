bda0059c769497f7d558751a66141ea5161571db
acetvertacova <acetvert@gmail.com>
2025-12-02 22:18:55 +0200

    remove default event type

--- src/main/java/com/tour_web_app/aop/annotation/LogEvent.java
+++ src/main/java/com/tour_web_app/aop/annotation/LogEvent.java
@@ -10,6 +10,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface LogEvent {
-    EventType eventType() default EventType.LOGIN;
+    EventType eventType();
     String description() default "";
 }
